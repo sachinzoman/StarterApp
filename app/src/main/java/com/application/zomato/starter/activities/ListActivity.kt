24a95c703eb5be2f1e64252f76c008991a1a3115
Sachin <sachin.b@zomato.com>
2019-06-25 17:41:43 +0530

    Sachin

--- app/src/main/java/com/application/zomato/starter/activities/ListActivity.kt
+++ app/src/main/java/com/application/zomato/starter/activities/ListActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import com.application.zomato.starter.R
 import com.application.zomato.starter.adapter.MyListAdapter
+import com.application.zomato.starter.data.Restaurant
 import kotlinx.android.synthetic.main.activity_list.*
 
 class ListActivity : AppCompatActivity() {
@@ -17,7 +18,17 @@ class ListActivity : AppCompatActivity() {
         /**
          * Change the initialisation to your type of list
          */
-        adapter.items = listOf("Item 1", "Item 2", "Item 3")
+
+        adapter.items = listOf(Restaurant("Rajinder Da Dhaba","Safdarjang","4.0"),
+                                Restaurant("Dominos","Dwarka Mor","3.7"),
+                                Restaurant("McDonalds","Paharganj","3.2"),
+                                Restaurant("Prem Dhaba","Karol Bagh","4.1"),
+                                Restaurant("Madira","Satiye Niketan","3.7"),
+                                Restaurant("Sita Ram Chhole","Chuna Mandi","4.5"),
+                                Restaurant(),
+                                Restaurant(),
+                                Restaurant(),
+                                Restaurant())
         /**
          * Very important operation to perform when data is changed
          */
---
+++ app/src/main/java/com/application/zomato/starter/data/Restaurant.kt
@@ -0,0 +1,14 @@
+package com.application.zomato.starter.data
+
+class Restaurant(
+
+//    constructor(restaurantName: String = "Prem Dhaba", restaurantAddress: String= "Karol Bagh", rating : String= "4.1") {
+//        this.restaurantName = restaurantName
+//        this.restaurantAddress = restaurantAddress
+//        this.rating = rating
+//    }
+
+    var restaurantName : String? = "Prem Dhaba",
+    var restaurantAddress : String? = "Karol Bagh",
+    var rating : String? = "4.1"){
+}
